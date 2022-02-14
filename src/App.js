8c6c8e2f43ce10f5812795d71b20b928e59d4dd1
Ceci <carbel.cecilia@gmail.com>
2022-02-14 12:42:44 +0100

    adding card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Card from './components/Card';
+//import Landing from './pages/Landing';
 
 const App = () => {
+
+  const[allPokemons, setAllPokemons] = useState([])
+   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    function createPokemon(results)  {
+      results.forEach( async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data =  await res.json()
+        setAllPokemons( currentList => [...currentList, data])
+        
+      
+      })
+    }
+    createPokemon(data.results)
+  await console.log(allPokemons)
+  }
+
+ useEffect(() => {
+  getAllPokemons()
+ }, [])
+
   return (
-    <div className="App">
-      pokeapi
+    <div className="app-container">
+     <h1>Pokemon API React</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+        {allPokemons.map( (pokemonIndex, index) => 
+            
+          
+        </div>
+          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
+      </div>
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({id, image, name, type}) => {
+    return(
+        <div className="card-container">
+            <div className="number"><small>#0{id}</small></div>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h3>{name}</h3>
+                <small>Type: {type}</small>
+            </div>
+        </div>
+    )
 
-}
+};
 export default Card;
