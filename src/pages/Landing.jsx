0444e539259dee926eae4953ab6ae60c8d894f61
Ceci <carbel.cecilia@gmail.com>
2022-02-14 11:10:04 +0100

    adding card

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState }from "react";
-
+import Card from "../components/Card";
 import axios from "axios";
 
 const Landing = () => {
 
     const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const getPokemon = async () => {
         let pokemonList = [];
@@ -13,10 +14,13 @@ const Landing = () => {
         }
         console.log(pokemonList);
         setPokemon(pokemonList);
+        setLoading(false);
 
     }
     const getPokemonData = async(id) => {
+        
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        console.log();
         return res;
     }
 
@@ -24,11 +28,26 @@ const Landing = () => {
         getPokemon();
     }, )
 
-    return(
+    const renderPokemon = () => {
+        if (loading) {
+          return <p>Loading...</p>;
+        }
+    
+    return pokemon.map(({ e }) => (
+        <Card
+        key={e.data.name}
+        
+        />
+    )); 
+    
+    };
+
+    return (
         <div>
-
+          <Card />
+          <div className="grid-container">{renderPokemon()}</div>
         </div>
-    );
-};
-
+      );
+   
+}
 export default Landing;
